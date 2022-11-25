1c4b658e737c36dbdf6a5f05ccf396cf6766425e
Pinglei He <pingleihe@outlook.com>
2022-11-26 00:40:10 +0100

    refactore code to add assertion helper function

--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -3,8 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("say hello to people when a name is supplied", func(t *testing.T) {
+		want := "Hello, Charles"
+		got := Hello("Charles")
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, world' when no string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
